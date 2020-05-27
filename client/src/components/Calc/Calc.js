c9d2f466ebf8c97a829c0f425a58914c21954c2e
michaelb <michaelpb@gmail.com>
2020-05-26 17:03:12 -0700

    Upgrading kc-react-widget

--- client/src/components/Calc/Calc.js
+++ client/src/components/Calc/Calc.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'kc-react-widgets';
+import { Button, Input } from 'kc-react-widgets';
 import './Calc.css';
 
 
@@ -29,16 +29,16 @@ function Calc(props) {
   return (
     <div className="Calc" style={style}>
       <p className="Calc-label">
-        <input
+        <Input
           value={props.label}
           onChange={ev => props.onLabelChange(ev.target.value)} />
       </p>
       <p className="Calc-type">{props.type}</p>
       <p className="Calc-value">
-        <input
+        <Input
           value={props.value}
           onChange={ev => onValueChange(ev.target.value)}
-        /> per <input
+        /> per <Input
           value={props.interval}
           onChange={ev => onIntervalChange(ev.target.value)}
         />
